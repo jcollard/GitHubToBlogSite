7ce7d81dbd91959cee41561e21ebe611db4131d5
jcollard <josephmcollard@gmail.com>
2022-11-30 13:55:13 +0000

    Deploying to gh-pages from  @ b7984d750038e00ecf00752e7c0ad2251950056c 🚀

---
+++ scripts/app.js
@@ -0,0 +1,30 @@
+import { initLoginAndSettingsModal } from "./user.js";
+import { db } from "./firebase.js";
+import { getDocs, collection } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js';
+import { formatTimeStamp } from "./common.js";
+
+async function loadAllArticles() {
+    const querySnapshot = await getDocs(collection(db, `articles`));
+    querySnapshot.forEach((doc) => {
+        displayArticle(doc);
+    });
+}
+
+function displayArticle(articleRef) {
+    const article = articleRef.data();
+    const parent = document.getElementById("articles");
+    const newCard = document.getElementById("article-template").cloneNode(true);
+    parent.appendChild(newCard);
+    newCard.querySelectorAll("#article-title")[0].innerHTML = article.name;
+    newCard.querySelectorAll("#article-published-at")[0].innerHTML = formatTimeStamp(article.publishedAt);
+    newCard.querySelectorAll("#article-description")[0].innerHTML = article.description;
+    newCard.querySelectorAll("#article-link")[0].href = `article.html?article-id=${articleRef.ref.id}`;
+    
+    newCard.style.display = "";
+    console.log(article);
+}
+
+document.body.onload = () => {
+    initLoginAndSettingsModal();
+    loadAllArticles();
+}
